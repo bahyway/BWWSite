70b5ccaa7193ec0f13a3d463fbfee56f6c757ddb
bahaa fadam <bfadam@bahyway.com>
2023-04-09 13:46:45 +0200

    Create vscode extensions file

---
+++ add-vscode-extensions.ps1
@@ -0,0 +1,48 @@
+$extensions =
+# Jekyll tools
+"neilding.language-liquid"
+, "killalau.vscode-liquid-snippets"
+# Docker , Microsoft Extension
+, "ms-azuretools.vscode-docker"
+# Remote Containers (Development)
+, "ms-vscode-remote.vscode-remote-extensionpack"
+# Git & GitHub extensions
+, "GitHub.copilot"
+, "GitHub.copilot-labs"
+, "GitHub.vscode-pull-request-github"
+, "GitHub.codespaces"
+, "GitHub.remotehub"
+, "henriqueBruno.github-repository-manager"
+, "Serhioromano.vscode-gitflow"
+, "mhutchie.git-graph"
+, "donjayamanne.githistory"
+, "eamodio.gitlens"
+, "arturock.gitstash"
+, "Stepsize.stepsize"
+, "me-dutour-mathieu.vscode-github-actions"
+, "sysoev.vscode-open-in-github"
+# Docker Extensions
+, "ms-azuretools.vscode-docker"
+, "formulahendry.docker-explorer"
+, "george3447.docker-run"
+, "p1c2u.docker-compose"
+, "TqrHsn.vscode-docker-registry-explorer"
+, "sandipchitale.docker-dive"
+
+
+$cmd = "code --list-extensions"
+Invoke-Expression $cmd -OutVariable output | Out-Null
+$installed = $output -split "\s"
+
+foreach ($ext in $extensions) {
+    if ($installed.Contains($ext)) {
+        Write-Host $ext "already installed." -ForegroundColor Gray
+    }
+    else {
+        Write-Host "Installing" $ext "..." -ForegroundColor White
+        code --install-extension $ext
+    }
+}
+
+
+
